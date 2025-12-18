2dc3d4b0afc4a8e4ac60c838e97a841236b62888
wu <wu@.com>
2025-12-18 14:49:08 +0800

    Build Test-micromatch/braces

--- test/spec.js
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'node:test';
-import { strict as assert } from 'node:assert';
-
-describe('npm-bump', () => {
-    it('dummy test', () => {
-        assert.strictEqual(true, true, 'dummy assertion');
-    });
-});
