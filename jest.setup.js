52e266ce0a8d1fe3b7fc3fd369cb0767dadf35ac
wu <wu@.com>
2025-12-18 15:16:07 +0800

    Build Test-rexxars/hyphenate-style-name

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// TODO: Lock to CDN distribution
-import recurly from 'recurly.js';
-
-global.recurly = recurly;
-
-Enzyme.configure({ adapter: new Adapter() });
