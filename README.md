a76952b98c3080fcfaf6a1c923655ad73b3d4f33
wu <wu@.com>
2026-01-05 21:36:23 +0800

    bdougherty/better-title-case

--- README.md
+++ README.md
@@ -1,19 +1,76 @@
-# asyncapi-directory
+# better-title-case
 
-Directory of asynchronous API specifications in [AsyncAPI](https://www.asyncapi.org/) format
+> Convert a string to title case based on the [Daring Fireball](https://daringfireball.net/2008/05/title_case) rules.
 
-## The APIs
+## Rules
 
-http://apis.guru/asyncapi-directory/APIs/
+- If the string is all-caps, it will be corrected
+- The following words are not capitalized by default: a, an, and, at, but, by, for, in, nor, of, on, or, so, the, to, up, yet, v, v., vs, and vs.
+- Words with capital letters other than the first are assumed to be capitalized properly and are skipped
+- It also skips any word that looks like a file path, file name, or URL
+- The first and last word are always capitalized
+- Sub-strings (those that are within quotes or parens/braces) are capitalized according to the same rules
 
-## Further information
+## Installation
 
-Check out the [About page](./docs/index.md)
+```
+$ npm install --save better-title-case
+```
 
-## REST APIs
+## Usage
 
-See also [OpenAPI-Directory](https://github.com/APIs-guru/openapi-directory)
+```js
+import titleCase from 'better-title-case';
+console.log(titleCase('Nothing to Be Afraid of?'));
+// Nothing to Be Afraid Of?
+```
 
-## Support
+## Advanced
 
-[![Become a backer](https://opencollective.com/openapi-directory/tiers/backer.svg?avatarHeight=36&width=600)](https://opencollective.com/openapi-directory)
+You can configure `better-title-case` to add your own excluded words to the default list, or to prevent the use of the default list by passing a `config` object as the second parameter.
+
+### excludedWords
+
+Type: `[string]`<br>
+Default: `[]`
+
+Additional words to exclude from capitalization.
+
+```js
+titleCase('Nothing to be afraid of?', {
+	excludedWords: ['be']
+});
+// 'Nothing to be Afraid Of?'
+```
+
+### useDefaultExcludedWords
+
+Type: `boolean`<br>
+Default: `true`
+
+Disable the usage of the default list of excluded words.
+
+```js
+titleCase('Nothing to be afraid of?', {
+	useDefaultExcludedWords: false
+});
+// 'Nothing To Be Afraid Of?'
+```
+
+### preserveWhitespace
+
+Type: `boolean`<br>
+Default: `false`
+
+Maintain extra whitespace between words. By default, all whitespace between words is collapsed to a single space.
+
+```js
+titleCase('Nothing  to be   afraid of?', {
+	preserveWhitespace: true
+});
+// 'Nothing  To Be   Afraid Of?'
+```
+
+## License
+
+MIT © [Brad Dougherty](https://brad.is)
--- index.js
+++ index.js
@@ -1,114 +1,129 @@
-#!/usr/bin/env node
+const alwaysLowercase = [
+	'a',
+	'an',
+	'and',
+	'at',
+	'but',
+	'by',
+	'for',
+	'in',
+	'nor',
+	'of',
+	'on',
+	'or',
+	'so',
+	'the',
+	'to',
+	'up',
+	'yet',
+	'v',
+	'v.',
+	'vs',
+	'vs.'
+];
 
-'use strict';
+const containers = new Set(['(', '[', '{', '"', `'`, '_']);
 
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+const isEmail = /.+@.+\..+/;
+const isFilePath = /^(\/[\w.]+)+/;
+const isFileName = /^\w+\.\w{1,3}$/;
+const hasInternalCapital = /(?![‑–—-])[a-z]+[A-Z].*/;
+const hasHyphen = /[‑–—-]/g;
 
-const fetch = require('node-fetch');
-const yaml = require('yaml');
-const sortObject = require('deep-sort-object');
-const recurse = require('reftools/lib/recurse').recurse;
-const jptr = require('reftools/lib/jptr').jptr;
-const docs = require('asyncapi-docgen');
-const Generator = require('@asyncapi/generator');
-const doc2 = new Generator('@asyncapi/html-template', path.resolve(__dirname, 'docs', 'html'), {
-  output: 'fs',
-  forceWrite: true,
-  templateParams: {
-    sidebarOrganization: 'byTags'
-  }
-});
-
-const colour = process.env.NODE_DISABLE_COLORS ?
-    { red: '', yellow: '', green: '', normal: '' } :
-    { red: '\x1b[31m', yellow: '\x1b[33;1m', green: '\x1b[32m', normal: '\x1b[0m' };
-
-const metadata = sortObject(yaml.parse(fs.readFileSync('./docs/_data/metadata.yaml','utf8')));
-
-function fixStr(str) {
-    str = str.split('\r\n').join('\n').split('\r').join('').split('---').join('###');
-    return str;
+function isUrl(url) {
+	try {
+		const parsed = new URL(url);
+		return Boolean(parsed.hostname);
+	} catch {
+		return false;
+	}
 }
 
-function fixApi(api) {
-    if (api.version && !api.asyncapi) {
-        api.asyncapi = api.version;
-        delete api.version;
-    }
-    if (api.info.version === '1.2.0') api.info.version = '1.0.0';
-    recurse(api,{},function(obj,key,state){
-        if (key === 'asyncapi_servers_variables') {
-            obj.variables = obj[key];
-            delete obj[key];
-        }
-        else if (key === '$ref') {
-            if (obj[key].startsWith('#/components/')) {
-                const found = jptr(api,obj[key]);
-                if (found === false) {
-                    console.warn('Not found',obj[key]);
-                    jptr(api,obj[key],{});
-                }
-            }
-        }
-    });
-    return api;
+function capitalize(string) {
+	if (string.length === 0) {
+		return string;
+	}
+
+	const letters = [...string];
+	const firstLetter = letters.shift();
+
+	if (containers.has(firstLetter)) {
+		return `${firstLetter}${capitalize(letters)}`;
+	}
+
+	return `${firstLetter.toUpperCase()}${letters.join('')}`;
 }
 
-async function main() {
-    return new Promise(async function(resolve,reject){
-        for (let api in metadata) {
-            console.log(colour.green+api+colour.normal);
-            for (let service in metadata[api]) {
-                console.log('  ',colour.yellow+service,metadata[api][service].origin+colour.normal);
-		        try {
-		            const res = await fetch(metadata[api][service].origin);
-                    let str = await res.text();
-                    str = fixStr(str);
-                    let obj = yaml.parse(str);
-                    obj = fixApi(obj);
-                    str = yaml.stringify(obj);
-                    let info;
-                    if (obj && obj.asyncapi && obj.info && obj.info.version) {
-                        info = obj.info;
-                        console.log('   @v'+info.version,info.title);
-                        const filename = api + (service === 'default' ? '' : '+'+service) + '@v' + info.version;
+export default function titleCase(
+	string = '',
+	{ excludedWords = [], useDefaultExcludedWords = true, preserveWhitespace = false } = {}
+) {
+	if (string.toUpperCase() === string) {
+		string = string.toLowerCase();
+	}
 
-                        fs.writeFile('./docs/APIs/'+filename+'.yaml',yaml.stringify(obj),'utf8',function(err){ if (err) console.warn(err.message) });
+	if (useDefaultExcludedWords) {
+		excludedWords.push(...alwaysLowercase);
+	}
 
-                        const header = { slug: filename, name: info.title, service: service, alpha: filename[0], layout: 'api', origin: metadata[api][service].origin, info: info, termsOfService: obj.termsOfService||'', externalDocs: obj.externalDocs||{}, stub: metadata[api][service].stub || false, tags: 'api' };
-                        fs.writeFileSync('APIs/'+filename+'.md','---\n'+yaml.stringify(header)+'\n---\n','utf8');
+	const words = string.trim().split(/(\s+)/);
 
-                        try {
-                            let html;
-                            if (obj.asyncapi.startsWith('1.')) {
-                                html = await docs.generateFullHTML(str);
-                                html = html.replace('main.css','main1.css');
-                                fs.writeFile('./docs/html/'+filename+'.html',html,'utf8',function(err){ if (err) console.warn(err.message) });
-                            }
-                            else if (obj.asyncapi.startsWith('2.')) {
-                                await doc2.generateFromString(str);
-                                fs.renameSync('./docs/html/index.html','./docs/html/'+filename+'.html');
-                            }
-                        }
-                        catch (ex) {
-                            console.warn('docs',ex.message||ex);
-                        }
-                    }
-                    else console.warn(util.inspect(obj));
-                }
-                catch (ex) {
-                    console.warn(ex);
-                }
-            }
-        }
-	    resolve(metadata);
-    });
+	const capitalizedWords = words.map((word, index, array) => {
+		if (/\s+/.test(word)) {
+			return preserveWhitespace ? word : ' ';
+		}
+
+		if (
+			isEmail.test(word) ||
+			isUrl(word) ||
+			isFilePath.test(word) ||
+			isFileName.test(word) ||
+			hasInternalCapital.test(word)
+		) {
+			return word;
+		}
+
+		const hyphenMatch = word.match(hasHyphen);
+
+		if (hyphenMatch) {
+			const isMultiPart = hyphenMatch.length > 1;
+			const [hyphenCharacter] = hyphenMatch;
+
+			return word
+				.split(hyphenCharacter)
+				.map((subWord) => {
+					if (isMultiPart && excludedWords.includes(subWord.toLowerCase())) {
+						return subWord;
+					}
+
+					return capitalize(subWord);
+				})
+				.join(hyphenCharacter);
+		}
+
+		if (word.includes('/')) {
+			return word
+				.split('/')
+				.map((part) => capitalize(part))
+				.join('/');
+		}
+
+		const isFirstWord = index === 0;
+		const isLastWord = index === words.length - 1;
+		const previousWord = index > 1 ? array[index - 2] : '';
+		const startOfSubPhrase = index > 1 && previousWord.endsWith(':');
+
+		if (
+			!isFirstWord &&
+			!isLastWord &&
+			!startOfSubPhrase &&
+			excludedWords.includes(word.toLowerCase())
+		) {
+			return word.toLowerCase();
+		}
+
+		return capitalize(word);
+	});
+
+	return capitalizedWords.join('');
 }
-
-console.log('Starting...');
-console.log('Entries in metadata.yaml:',Object.keys(metadata).length);
-main().then(function(){
-  console.log('Finishing...');
-});
